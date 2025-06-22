763f22c66762e0375e88ffcb5fa14ab96e8ceca1
bhvaycan <bhavy203@gmail.com>
2025-06-21 23:25:41 -0600

    build service ia added

--- src/components/services/BuildService.jsx
+++ src/components/services/BuildService.jsx
@@ -3,13 +3,17 @@ import BuildCard from '../../templates/BuildCard'
 
 const BuildService = () => {
   return (
-    <div className='w-full h-[100vh] flex items-center justify-center bg-green-600'>
-            <div className="title bg-blue-500">
-                <h1>Brick</h1>
-                <h1>By</h1>
-                <h1>Brick</h1>
+    <div className='w-full h-[100vh] flex items-center justify-center  relative'>
+            <div className="title w-[60%] flex flex-col items-center justify-center px-[10%] h-[80%]">
+                <h1 className='w-full h-[30%]   text-[10vw] flex items-center font-["Seasons"]'>Brick</h1>
+                <h1 className='w-full h-[2%] text-gray-500 font-bold text-[5vw]  flex items-center justify-center font-["Brittany"]'>By</h1>
+                <h1 className='w-full h-[30%] text-[10vw] flex items-center ml-[15%] justify-end font-["Seasons"]'>Brick</h1>
             </div>
-        {/* <BuildCard /> */}
+
+            <div className='w-full h-full flex items-center justify-center absolute'>
+                <BuildCard />
+            </div>
+        
     </div>
   )
 }
--- src/templates/BuildCard.jsx
+++ src/templates/BuildCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BuildCard = () => {
   return (
-    <div className='w-[25vw] h-[60vh] bg-white px-3 py-3 rounded-xl'>
+    <div className='w-[25vw] h-[60vh] bg-zinc-400 px-3 py-3 rounded-xl'>
         <div className="indside-box w-[100%] h-[85%] overflow-hidden bg-red-300 rounded-3xl">
             <img loading='lazy' className='w-full h-full object-cover' src="/images/livingroom.png" alt="" />
         </div>
