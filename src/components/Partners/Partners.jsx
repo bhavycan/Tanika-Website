3d08c84ae1b7a203d3cd30b1fa03ad5cc1e87203
bhvaycan <bhavy203@gmail.com>
2025-06-24 22:48:28 -0600

    changing partner component

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import HomePartners from './HomePartners'
 import PartnerFlipCards from './PartnerFlipCards'
+import SubPartners from './SubPartners'
 
 const Partners = () => {
   return (
     <div className='w-full min-h-[100vh]'>
         <HomePartners />
-        <PartnerFlipCards />
+        <SubPartners />
     </div>
   )
 }
