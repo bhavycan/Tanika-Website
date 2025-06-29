aceb043d327285de910b8161f2288cdc6c70077f
bhvaycan <bhavy203@gmail.com>
2025-06-29 15:00:39 -0600

    navbar changed

--- src/templates/NavBar.jsx
+++ src/templates/NavBar.jsx
@@ -1,31 +1,33 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import useIsMobile from '../hooks/useIsMobile';
+import { div } from 'motion/react-client';
 
 const NavBar = () => {
   const navigate = useNavigate();
   const isMobile = useIsMobile();
+  
 
   return (
-    <motion.nav
+   <motion.nav
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       viewport={{ once: true }}
       transition={{ duration: 1, ease: 'easeIn' }}
       role="navigation"
       aria-label="Main Navigation"
-      className="w-full h-[8vh] flex items-center justify-center fixed bottom-[5%] left-0 z-[9999] mt-[1%] px-2 py-2"
+      className={`w-full h-[8vh] flex items-center justify-center fixed ${isMobile ? "-bottom-[3%]" : "bottom-[0%]  px-2 py-2"}  left-0 z-[9999] mt-[1%]`}
     >
       <div
         className={`navbar ${
-          isMobile ? 'w-[100%]' : 'w-[80%]'
-        } h-full shadow-2xl rounded-full flex items-center justify-between gap-[2%] px-[2%] py-2 font-serif relative backdrop-blur-lg`}
+          isMobile ? 'w-[100%]' : 'w-[40%]'
+        } h-full shadow-2xl  flex  justify-between gap-[2%] px-[2%] py-2 font-serif relative backdrop-blur-lg rounded-full`}
       >
         <button
           onClick={() => navigate(-1)}
           aria-label="Go Back"
-          className="logo w-[8%] h-[100%] flex items-center justify-center bg-zinc-200 rounded-full shadow-2xl"
+          className={`logo w-[8%] items-center  ${isMobile ? "h-[50%]" : "h-[100%] "} flex  justify-center bg-zinc-200 rounded-full shadow-2xl`}
         >
           <i className="ri-arrow-left-line" aria-hidden="true"></i>
         </button>
@@ -37,6 +39,7 @@ const NavBar = () => {
         <Link to="/faqs" aria-label="Frequently Asked Questions">FAQs</Link>
       </div>
     </motion.nav>
+    
   );
 };
 
